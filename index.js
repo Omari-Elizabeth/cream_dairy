076e9a5a292db57d87df528284b3f2369e099625
Omari-Elizabeth <elizabeth.omari@student.moringaschool.com>
2022-07-17 23:19:07 +0300

    delete empty file

---
+++ index.js
@@ -0,0 +1,8 @@
+var shedName = []
+function totalProduction()
+        {
+            var num1 = parseInt(document.getElementById("Num1").value);
+            var num2 = parseInt(document.getElementById("Num2").value);
+            document.getElementById("Sum").value = num1 + num2;
+
+        }
