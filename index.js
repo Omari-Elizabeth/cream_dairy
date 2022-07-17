104985123095f4f19917fc7c119bd5584c0a20a9
Omari-Elizabeth <elizabeth.omari@student.moringaschool.com>
2022-07-18 00:13:01 +0300

    add function to get and save in array

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ var shedName = new Array();
 var period = new Array();
 var production = new Array();
 
+function saveData()
+        {
+            var shedName = document.getElementById('shed_id').value;
+            var period = document.getElementById('date').value;
+            var production = document.getElementById('litres').value;
+        }
 function totalProduction()
         {
             var num1 = parseInt(document.getElementById("Num1").value);
