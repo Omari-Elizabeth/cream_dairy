dafb9a679fffae3f926cfc38ef34615c58842c6b
Omari-Elizabeth <elizabeth.omari@student.moringaschool.com>
2022-07-18 15:35:19 +0300

    js update

--- index.js
+++ index.js
@@ -7,7 +7,15 @@ function saveData()
             var shedName = document.getElementById('shed_id').value;
             var period = document.getElementById('date').value;
             var production = document.getElementById('litres').value;
+            shedName[shedName.length]=shed_id;
+            period[period.length]=date;
+            production[production.length]=litres;
         }
+function giveOutput()
+        {
+            var content = "Your Production for shed (content+=[...shedName]) is (content+=[...production]"); 
+        }
+        
 function totalProduction()
         {
             var num1 = parseInt(document.getElementById("Num1").value);
